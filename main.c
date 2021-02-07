0821f29f065f3d9292d36313480f5e39388c8e05
pawans1994 <pawan.p.121@gmail.com>
2021-02-06 22:51:59 -0500

    Error Handling

--- main.c
+++ main.c
@@ -1,13 +1,3 @@
-/******************************************************************************
- *
- *  File Name........: main.c
- *
- *  Description......: Simple driver program for ush's parser
- *
- *  Author...........: Vincent W. Freeh
- *
- *****************************************************************************/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -65,23 +55,23 @@ static void exCmd(char cmd[1024], char  **cmdArgs, char *out_file, int fd, char 
             close(file_desc);
         }
         else
+        {
+            if (fd != 0)
             {
-                if (fd != 0)
-                {
-                    if (strcmp(flag, "a") == 0) {
-                        file_desc = open(out_file, O_RDWR | O_APPEND | O_CREAT, 664);
-                    }
-                    if (strcmp(flag, "w") == 0) {
-                        file_desc = open(out_file, O_WRONLY | O_CREAT | O_TRUNC, 0666);
-                    }
-                    if(file_desc < 0){
-                        perror("Couldn't open file");
-                        exit(0);
-                    }
-                    dup2(file_desc, 1);
-                    close(file_desc);
+                if (strcmp(flag, "a") == 0) {
+                    file_desc = open(out_file, O_RDWR | O_APPEND | O_CREAT, 664);
                 }
+                if (strcmp(flag, "w") == 0) {
+                    file_desc = open(out_file, O_WRONLY | O_CREAT | O_TRUNC, 0666);
+                }
+                if(file_desc < 0){
+                    perror("Couldn't open file");
+                    exit(0);
+                }
+                dup2(file_desc, 1);
+                close(file_desc);
             }
+        }
 
         execvp(cmd, cmdArgs);
 //        perror("execvp");
@@ -102,10 +92,12 @@ static void ch_dir(char *cmd,  char *path){
         getcwd(new_path, sizeof(new_path));
         strcat(new_path, "/");
         strcat(new_path, path);
-        chdir(new_path);
+        if(chdir(new_path) != 0)
+            perror("Error");
     }
     else{
-        chdir(path);
+        if(chdir(path) != 0)
+            perror("Error");
     }
 }
 
@@ -118,131 +110,132 @@ static void prCmd(Cmd c)
     char *flag;
     int in;
     bool isPipe = 0;
-  if ( c ) {
-    //printf("%s%s ", c->exec == Tamp ? "BG " : "", c->args[0]);
-    if ( c->in == Tin ) {
-        in = 1;
-        //printf("<(%s) ", c->infile);
-    }
-    if ( c->out != Tnil )
-    {   in = 0;
-      switch ( c->out ) {
-      case Tout:
-	//printf(">(%s) ", c->outfile);
-	fd = 1;
-    flag = malloc(10*sizeof (char*));
-	strcpy(flag, "w");
-	break;
-      case Tapp:
-      fd = 1;
-      flag = malloc(10*sizeof (char*));
-      strcpy(flag, "a");
-	//printf(">>(%s) ", c->outfile);
-	break;
-      case ToutErr:
-	//printf(">&(%s) ", c->outfile);
-	fd = 2;
-    flag = malloc(10*sizeof (char*));
-    strcpy(flag, "w");
-	break;
-      case TappErr:
-	//printf(">>&(%s) ", c->outfile);
-	fd = 2;
-	strcpy(flag, "a");
-	break;
-      case Tpipe:
-    //printf("| ");
-    pipe_flag = 1;
-    isPipe= 1;
-    break;
-      case TpipeErr:
-	//printf("|& ");
-	break;
-      default:
-	fprintf(stderr, "Shouldn't get here\n");
-	exit(-1);
-      }}
-    //Writing the commands from data structure and it's arguments to variable cmd and cmdArgs
-      cmdArgs = malloc(1024 * sizeof(char*));
-      strcpy(cmd, c->args[0]);
-      cmdArgs[0] = malloc(1024* sizeof(char));
-      strcpy(cmdArgs[0], c->args[0]);
-    if ( c->nargs > 1 ) {
+    if ( c ) {
+        //printf("%s%s ", c->exec == Tamp ? "BG " : "", c->args[0]);
+        if ( !strcmp(c->args[0], "end") )
+            exit(0);
+        if ( c->in == Tin ) {
+            in = 1;
+            //printf("<(%s) ", c->infile);
+        }
+        if ( c->out != Tnil )
+        {   in = 0;
+            switch ( c->out ) {
+                case Tout:
+                    //printf(">(%s) ", c->outfile);
+                    fd = 1;
+                    flag = malloc(10*sizeof (char*));
+                    strcpy(flag, "w");
+                    break;
+                case Tapp:
+                    fd = 1;
+                    flag = malloc(10*sizeof (char*));
+                    strcpy(flag, "a");
+                    //printf(">>(%s) ", c->outfile);
+                    break;
+                case ToutErr:
+                    //printf(">&(%s) ", c->outfile);
+                    fd = 2;
+                    flag = malloc(10*sizeof (char*));
+                    strcpy(flag, "w");
+                    break;
+                case TappErr:
+                    //printf(">>&(%s) ", c->outfile);
+                    fd = 2;
+                    strcpy(flag, "a");
+                    break;
+                case Tpipe:
+                    //printf("| ");
+                    pipe_flag = 1;
+                    isPipe= 1;
+                    break;
+                case TpipeErr:
+                    //printf("|& ");
+                    break;
+                default:
+                    fprintf(stderr, "Shouldn't get here\n");
+                    exit(-1);
+            }}
+        //Writing the commands from data structure and it's arguments to variable cmd and cmdArgs
+        cmdArgs = malloc(1024 * sizeof(char*));
+        strcpy(cmd, c->args[0]);
+        cmdArgs[0] = malloc(1024* sizeof(char));
+        strcpy(cmdArgs[0], c->args[0]);
+        if ( c->nargs > 1 ) {
 
 //      printf("[");
-      for ( i = 1; c->args[i] != NULL; i++ ){
-            cmdArgs[i] = malloc(1024* sizeof(char));
-            strcpy(cmdArgs[i], c->args[i]);
+            for ( i = 1; c->args[i] != NULL; i++ ){
+                cmdArgs[i] = malloc(1024* sizeof(char));
+                strcpy(cmdArgs[i], c->args[i]);
 //            printf("%d:%s,", i, c->args[i]);
-      }
+            }
 //      printf("\b]");
-    }
-    //Executing the command
-    if(pipe_flag == 0)
-    {
-        if (strcmp(c->args[0], "cd") == 0) {
-            ch_dir(cmd, cmdArgs[1]);
         }
-        else{
+        //Executing the command
+        if(pipe_flag == 0)
+        {
+            if (strcmp(c->args[0], "cd") == 0) {
+                ch_dir(cmd, cmdArgs[1]);
+            }
+            else{
 //        {   printf("%s", cmd);
-            if (in == 0)
-                exCmd(cmd, cmdArgs, c->outfile, fd, flag, in);
-            else
-                exCmd(cmd, cmdArgs, c->infile, fd, flag, in);
+                if (in == 0)
+                    exCmd(cmd, cmdArgs, c->outfile, fd, flag, in);
+                else
+                    exCmd(cmd, cmdArgs, c->infile, fd, flag, in);
+            }
         }
-    }
-    else {
-       if(isPipe == 1) {
-           pipe(pfd);
-           run_pipe(cmd, cmdArgs, in_desc, pfd[1]);
-           close(pfd[1]);
-           in_desc = pfd[0];
-       }
-       else
-       {
-           run_pipe(cmd, cmdArgs, in_desc, 1);
-           pipe_flag = 0;
-       }
+        else {
+            if(isPipe == 1) {
+                pipe(pfd);
+                run_pipe(cmd, cmdArgs, in_desc, pfd[1]);
+                close(pfd[1]);
+                in_desc = pfd[0];
+            }
+            else
+            {
+                run_pipe(cmd, cmdArgs, in_desc, 1);
+                pipe_flag = 0;
+            }
+
+        }
+        //putchar('\n');
+
+        // this driver understands one command
 
     }
-    putchar('\n');
-
-    // this driver understands one command
-    if ( !strcmp(c->args[0], "end") )
-      exit(0);
-  }
 }
 
 
 static void prPipe(Pipe p)
 {
-  int i = 0;
-  Cmd c;
+    int i = 0;
+    Cmd c;
 
-  if ( p == NULL )
-    return;
+    if ( p == NULL )
+        return;
 
-  //printf("Begin pipe%s\n", p->type == Pout ? "" : " Error");
-  for ( c = p->head; c != NULL; c = c->next ) {
-    //printf("Cmd #%d: ", ++i);
-    prCmd(c);
-  }
-  //printf("\nEnd pipe\n");
-  prPipe(p->next);
+    //printf("Begin pipe%s\n", p->type == Pout ? "" : " Error");
+    for ( c = p->head; c != NULL; c = c->next ) {
+        //printf("Cmd #%d: ", ++i);
+        prCmd(c);
+    }
+    //printf("\nEnd pipe\n");
+    prPipe(p->next);
 }
 
 int main(int argc, char *argv[])
 {
-  Pipe p;
+    Pipe p;
 
-  char *host = "armadillo";
+    char *host = "armadillo";
 
-  while ( 1 ) {
-    printf("%s%% ", host);
-    p = parse();
-    prPipe(p);
-    freePipe(p);
-  }
+    while ( 1 ) {
+        printf("%s%% ", host);
+        p = parse();
+        prPipe(p);
+        freePipe(p);
+    }
 }
 
 /*........................ end of main.c ....................................*/
